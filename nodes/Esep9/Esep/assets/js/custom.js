c06a0d679b0db2056caacadb8720866d020eb76b
MadikMayCry <d1e.4.liberty@gmail.com>
2018-02-01 12:17:16 +0700

    second

--- nodes/Esep9/Esep/assets/js/custom.js
+++ nodes/Esep9/Esep/assets/js/custom.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    var p1, temp1, lambda, k = 1.41, r = 287, ktemp1, volume1, e = 6, volume2v = 0.19, p2 = 1.25, ktemp2 = 833.88, volume3 = 0.19, p3 = 4.37, ktemp3 = 2918.58 , volume4 = 1.15, ktemp4 = 1400, p4 = 0.35, cv = 20.9, q1, q2 = 720.69, q0, ut0, utkarno ;
+    var p1 = 0.1, temp1 = 20, e = 12.7, k = 1.4, r = 287, volume1 = 0.84, volume2 = 0.07;
     var allCorrect = false;
     var correctCount = 0;
     var correctArrays = [];
 
     $("#userForms").submit(function(event) {
-    //Variables
+        //Variables
         correctArrays = new Array($('.user_input_field.active input[type="text"]').length);
         correctArrays.fill(0);
         
@@ -25,7 +25,7 @@ $(document).ready(function() {
                 var input = $(this);
                 var inputId = input.attr("id");
                 var inputVal = input.val();
-                var values = { "userInput1": volume2v, "userInput2": p2, "userInput3": ktemp2 ,"userInput4": volume3,"userInput5": p3,"userInput6": ktemp3, "userInput7": volume4, "userInput8": ktemp4, "userInput9": p4, "userInput10": q1};
+                var values = { "userInput1": volume1, "userInput2": volume2};
 
                 
 
@@ -59,37 +59,12 @@ $(document).ready(function() {
     });
 
     function generateRandom() {
-        p1 = Math.round(((Math.random() * 0.5) + 0.1 ) * 100) / 100; 
-        temp1 =Math.floor(Math.random() * 100) + 100;
-        lambda = Math.round(((Math.random() * 5.5) + 1.5 ) * 10) / 10; 
-        
+        p1 = Math.round(((Math.random() * 1.5)) * 100) / 100; 
+        temp1 = Math.floor((Math.random() * 200) + 250);
         ktemp1 = temp1 + 273;
-        
-        //Step 1
-        volume1 = Math.round(r * ktemp1/ Math.pow(10,5) * 100) / 100;
-
-        volume2v = Math.round(volume1/e *100)/100;
-        p2 =  Math.round(p1 * Math.pow(e,k) * 100) / 100;
-        ktemp2 = Math.round(ktemp1 * Math.pow(e, k-1) * 100) / 100;
-        
-        //Step 2
-        volume3 = volume2v;
-        p3 = Math.round(p2 * lambda * 100) / 100;
-        ktemp3 = Math.round(ktemp2 * p3/ p2 * 100) / 100; 
-
-        //Step 3
-        volume4 = volume1;
-        ktemp4 = Math.round(ktemp3 / Math.pow(e, k-1) * 100) / 100;
-        p4 = Math.round((r * ktemp4) / (volume4 * 1000000) * 100) / 100;
-
-        //Step 4
-        q1 = Math.round(cv * (ktemp3 - ktemp2)/ 2 * 100) / 100;
-        q2 = Math.round(cv * (ktemp4 - ktemp1)/ 29 * 100) / 100;
-        q0 = Math.round((q1 - q2) * 100) / 100;
-
-        ut0 = Math.round(q0/q1 * 100) / 100;
-
-        utkarno = Math.round((1 - ktemp1/ ktemp3) * 100) / 100 ; 
+        volume1 = Math.round(r * ktemp1 / (p1 * Math.pow(10, 6)) * 100) / 100;
+        volume2 = Math.round( volume1 / e * 100) / 100;
+        e = Math.round(((Math.random() * 5) + 10) * 100) / 100; 
 
         initalize();
     }
@@ -97,11 +72,8 @@ $(document).ready(function() {
     function initalize() {
         $(".p1").html(p1);
         $(".temp1").html(temp1);
-        $(".lambda").html(lambda);
-        console.log("Step 1 " + volume1, volume2v, p2, ktemp2);
-        console.log("Step 2 " + volume3, p3, ktemp3);
-        console.log("Step 3 " + volume4, ktemp4, p4);
-        console.log("Step 4 " + q1, q2, q0, ut0, utkarno);
+        $(".e").html(e);
+        console.log(volume1, volume2);
     }
 
     function wrongInput(argument) {
